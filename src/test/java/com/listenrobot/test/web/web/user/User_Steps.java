b511317fc1747d73a666755a0aa68350c992eda6
董轶 <dongyi@mudu.tv>
2020-09-15 10:14:07 +0800

    security update

--- src/test/java/com/listenrobot/test/web/web/user/User_Steps.java
+++ src/test/java/com/listenrobot/test/web/web/user/User_Steps.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+import java.util.Map;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -19,9 +22,13 @@ public class User_Steps {
 
     @Test
     public void test() {
-        restAssured.given().pathParam("issueId", "MDCS-1183").queryParam("expand", "changelog")
+        List jsonList = restAssured.given().pathParam("issueId", "MDCS-1264").queryParam("expand", "changelog")
                 .when().get("/rest/api/2/issue/{issueId}")
-                .then().log().all().statusCode(200);
+                .then().statusCode(200)
+                .extract().body().jsonPath().get("changelog.histories");
+        Map jsonMap = (Map) jsonList.get(jsonList.size() - 1);
+        List itemsList = (List) jsonMap.get("items");
+        System.out.println(itemsList.get(itemsList.size()-1));
     }
 
 

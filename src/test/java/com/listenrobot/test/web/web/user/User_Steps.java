dc6986cdfc5c08cb63be8aeef250fd75f9c76f9b
董轶 <dongyi@mudu.tv>
2020-09-15 18:01:58 +0800

    update case

--- src/test/java/com/listenrobot/test/web/web/user/User_Steps.java
+++ src/test/java/com/listenrobot/test/web/web/user/User_Steps.java
@@ -10,10 +10,7 @@ import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,30 +20,36 @@ public class User_Steps {
 
     JiraFlow jiraFlow = new JiraFlow();
 
-    @Test
-    public void test() {
-        List<Map<String,Object>> responseJsonList = RestAssured.given().pathParam("issueId", "MDCS-819").queryParam("expand", "changelog")
+    public Boolean isIssueImpacted(String issueId) {
+        List<Map<String, Object>> responseJsonList = RestAssured
+                .given().pathParam("issueId", issueId).queryParam("expand", "changelog")
                 .when().get("/rest/api/2/issue/{issueId}")
                 .then().statusCode(200)
                 .extract().body().jsonPath().get("changelog.histories");
-        System.out.println(jiraFlow.isImpactIssue(responseJsonList));
-
+        return jiraFlow.isImpactIssue(responseJsonList);
     }
 
     @Test
-    public void getIssueList() {
+    public void getIssueListFromJQL() {
         List<String> fields = Collections.singletonList("key");
         Map<String, Object> searchParameter = new HashMap<>();
         searchParameter.put("jql", "project = MDCS AND issuetype = 故障 AND 缺陷严重等级 =  无");
         searchParameter.put("startAt", 0);
-        searchParameter.put("maxResults", 10);
+        searchParameter.put("maxResults", 50);
         searchParameter.put("fields", fields);
-        List<Map<String,Object>> issueList = RestAssured.given().body(searchParameter)
+        List<Map<String, Object>> issueList = RestAssured
+                .given().body(searchParameter)
                 .when().post("/rest/api/2/search")
                 .then().assertThat().statusCode(HttpStatus.SC_OK)
                 .extract().body().jsonPath().get("issues");
         List<String> keyList = jiraFlow.compositeKeyList(issueList);
-        System.out.println(keyList);
+        List<String> impactKeyList = new ArrayList<>();
+        keyList.forEach(key -> {
+            if(isIssueImpacted(key)){
+                impactKeyList.add(key);
+            }
+        });
+        System.out.println(impactKeyList);
     }
 
 
